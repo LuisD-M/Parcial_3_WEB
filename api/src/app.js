f99cff1d7ff9f2ab782d1ba76e04556b28de4900
Luis Muñoz <luizda750@gmail.com>
2024-11-15 21:25:15 -0500

    Dependencias

--- api/src/app.js
+++ api/src/app.js
@@ -4,9 +4,8 @@ import * as swaggerUi from "swagger-ui-express";
 
 import { setContentType } from "./middleware/authMiddleware.mjs";
 
-import { MedicoRoutes } from "./routes/MedicoRoutes.mjs";            //Se importa todas las clases
+import { MedicoRoutes } from "./routes/MedicoRoutes.mjs";            // Se importa la clase MedicoRoutes
 import { PacienteRoutes } from "./routes/PacienteRoutes.mjs";
-
 
 const app = express();
 
@@ -31,12 +30,12 @@ app.get("/api-docs.json", (req, res) => {
 app.use(express.json());
 app.use(setContentType);
 
-const MedicoRoutes = new MedicoRoutes();                //crean instancias
-const PacienteRoutes = new PacienteRoutes();
+// Cambié el nombre de la instancia a `medicoRoutes` para evitar el conflicto
+const medicoRoutes = new MedicoRoutes();
+const pacienteRoutes = new PacienteRoutes();
 
-app.use("/Medico", MedicoRoutes.router);
-app.use("/Paciente", PacienteRoutes.router);
-
+app.use("/Medico", medicoRoutes.router);  // Usa la instancia `medicoRoutes`
+app.use("/Paciente", pacienteRoutes.router);  // Usa la instancia `pacienteRoutes`
 
 app.all("*", (req, res) => {
   res.status(404).send(JSON.stringify({ message: "invalid path" }));
@@ -46,15 +45,10 @@ app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send("Something broke!");
 });
-// expressOasGenerator.handleRequests();
+
 app.listen(8080, () => {
   console.log("Servidor escuchando en el puerto 8080");
 });
-
-
-
-
-
 
 
 
